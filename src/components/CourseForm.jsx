9c59fe8da05de175d404067f54522b0c61b311f0
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-03-25 12:21:14 +0530

    init

---
+++ src/components/CourseForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CourseForm = () => {
+  return (
+    <div className="form-container">
+      <input className="form-input" />
+      <button className="form-submit-btn">Add Course</button>
+    </div>
+  );
+};
+
+export default CourseForm;
---
+++ src/components/CourseList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CourseList = () => {
+  return (
+    <>
+      <ul></ul>
+    </>
+  );
+};
+
+export default CourseList;
