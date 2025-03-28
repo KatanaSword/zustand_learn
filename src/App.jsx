7168492e70c0b3db93df3409516378191d1bb89e
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-03-28 19:27:27 +0530

    setup app for render code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import "./App.css";
+import "./index.css";
+import CourseForm from "./components/CourseForm";
+import CourseList from "./components/CourseList";
 
 function App() {
-  return <></>;
+  return (
+    <div className="main-container">
+      <h1
+        style={{
+          fontSize: "2.3rem",
+          marginBottom: "2rem",
+        }}
+      >
+        {" "}
+        My Course List{" "}
+      </h1>
+      <CourseForm />
+      <CourseList />
+    </div>
+  );
 }
 
 export default App;
