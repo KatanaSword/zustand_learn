564cfa8fa455bf4470b7ac3ffd37876fe16b0e02
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-03-28 19:24:31 +0530

    feat: course form

--- src/components/CourseForm.jsx
+++ src/components/CourseForm.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import useCourseStore from "../app/courseStore";
 
 const CourseForm = () => {
+  const addCourse = useCourseStore((state) => state.addCourse);
+
+  const [courseTitle, setCourseTitle] = useState("");
+  console.log("CourseForm Rendered");
+
+  const handleCourseSubmit = () => {
+    if (!courseTitle) return alert("Please add a course title");
+    addCourse({
+      id: Math.ceil(Math.random() * 1000000),
+      title: courseTitle,
+    });
+    setCourseTitle("");
+  };
   return (
     <div className="form-container">
-      <input className="form-input" />
-      <button className="form-submit-btn">Add Course</button>
+      <input
+        className="form-input"
+        value={courseTitle}
+        onChange={(e) => setCourseTitle(e.target.value)}
+      />
+      <button className="form-submit-btn" onClick={() => handleCourseSubmit()}>
+        Add Course
+      </button>
     </div>
   );
 };
