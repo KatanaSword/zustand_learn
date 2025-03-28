45dc27691a50c935980551fa1166008fbe1419a0
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-03-28 19:24:51 +0530

    feat: course list

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import useCourseStore from "../app/courseStore";
+import { useShallow } from "zustand/shallow";
 
 const CourseList = () => {
+  const { courses, removeCourse, toggleCourseStatus } = useCourseStore(
+    useShallow((state) => ({
+      courses: state.courses,
+      removeCourse: state.removeCourse,
+      toggleCourseStatus: state.toggleCourseStatus,
+    }))
+  );
   return (
     <>
-      <ul></ul>
+      <ul>
+        {courses.map((course, i) => {
+          return (
+            <React.Fragment key={i}>
+              <li
+                className={`course-item`}
+                style={{
+                  backgroundColor: course.completed ? "#00FF0044" : "white",
+                }}
+              >
+                <span className="course-item-col-1">
+                  <input
+                    checked={course.completed}
+                    type="checkbox"
+                    onChange={(e) => {
+                      toggleCourseStatus(course.id);
+                    }}
+                  />
+                </span>
+                <span
+                  style={{
+                    color: "black",
+                  }}
+                >
+                  {course?.title}
+                </span>
+                <button
+                  className="delete-btn"
+                  onClick={() => removeCourse(course.id)}
+                >
+                  Delete
+                </button>
+              </li>
+            </React.Fragment>
+          );
+        })}
+      </ul>
     </>
   );
 };
